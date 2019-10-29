c728dc8ffb7f6eb97523f902a133b5e1227f5389
Jolene Poulin <jcpoulin@ualberta.ca>
2019-10-28 22:58:51 -0600

    Added Formspree integration and commented out straight PHP implementation of contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 if(isset($_POST['submit'])){
     $to = "jcpoulin@ualberta.ca"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
@@ -15,4 +15,4 @@ if(isset($_POST['submit'])){
     echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-?>
+?> -->
